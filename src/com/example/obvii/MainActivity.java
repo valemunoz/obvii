1d1a159b61fc63b6ec162ff1ff20de396f693e0e
valemunoz <valemunoz@gmail.com>
2014-09-05 15:53:21 -0400

    obvii app nativa android

---
+++ src/com/example/obvii/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.obvii;
+
+import android.os.Bundle;
+//import android.app.Activity;
+import org.apache.cordova.*;
+
+
+public class MainActivity extends DroidGap 
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		super.setIntegerProperty("splashscreen", R.drawable.screen);
+		super.loadUrl("file:///android_asset/www/index.html",30000);
+		//super.loadUrl("file:///android_asset/www/index_nob.html");
+		
+	     
+		
+	}
+}
+
